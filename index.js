57e8b1c4ace4f3e832ee66236e8c82e711245a23
Abhishek khanna <ABHISHEKKHANNA1995@GMAIL.COM>
2021-11-15 23:32:07 +0530

    Implement payment api endpoints (#3)
    * feat: updated the version number
    
    * feat: implement GET methods for /orders/:orderId and /orders/history
    
    * feat: implement CRUD methods for /payments

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ app.use(express.json());
 
 // Routes
 const orders = require("./routes/orders");
+const payments = require("./routes/payments");
 
 app.get("/", (req, res) => {
   res.send("Zoggy v1.0.1");
 });
 
 app.use("/api/orders", orders);
+app.use("/api/payments", payments);
 
 app.listen(process.env.PORT, () => {
   console.log(`Project running at port ${process.env.PORT}`);
