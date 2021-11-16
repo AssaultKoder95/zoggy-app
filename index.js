ef095a26f857a6dbfe340c139f0e199c43de6ac7
Abhishek Khanna <Abhishekkhanna1995@gmail.com>
2021-11-16 22:08:07 +0530

    Project Restructuring Completed

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const path = require("path");
+
+const public = path.join(__dirname, "public");
 
 // eg of in-built middleware
 app.use(express.json());
@@ -12,21 +15,21 @@ app.use(express.json());
 // => req.params => { app: 1, version: 2, userId: 4 }
 
 // Routes Imported
-const orders = require("./routes/orders");
-const payments = require("./routes/payments");
+const apiRoutes = require("./routes");
 
 // Middlewares Imported
-const { logger, auth } = require("./middlewares");
+const { logger } = require("./middlewares");
 
 // Route Handling
 app.use(logger);
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Zoggy v1.0.1");
 });
 
-app.use("/api/orders", orders);
-app.use("/api/payments", auth, payments);
+app.use("/api", apiRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Project running at port ${process.env.PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const Router = express.Router();
 
+const { auth } = require("../middlewares");
+
 const orders = require("./orders");
 const payments = require("./payments");
+const dishes = require("./dishes");
 
-Router.use("/api/orders", orders);
-Router.use("/api/payments", auth, payments);
+Router.use("/orders", orders);
+Router.use("/payments", auth, payments);
+Router.use("/dishes", auth, dishes);
+
+module.exports = Router;
